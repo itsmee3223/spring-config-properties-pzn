414d03a6bd7819214ba367186ae9996903f0d735
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-20 12:26:21 +0700

    external properties file

--- src/main/java/ramanda/spring/config/properties/Application.java
+++ src/main/java/ramanda/spring/config/properties/Application.java
@@ -2,10 +2,20 @@ package ramanda.spring.config.properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.context.annotation.Bean;
+import ramanda.spring.config.properties.converter.StringToDateConverter;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class Application {
-
+	@Bean
+	public ApplicationConversionService conversionService(StringToDateConverter stringToDateConverter){
+		ApplicationConversionService applicationConversionService = new ApplicationConversionService();
+		applicationConversionService.addConverter(stringToDateConverter);
+		return applicationConversionService;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
